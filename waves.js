870cc93eca44dae61e2ebb6226c05566b4ee727f
Bjorn Tipling <bjorn@ambientchill.com>
2014-09-20 22:07:26 -0700

    Safer, split on any whitespace, cleanups.
    Escaping dangerous XSS problems with clean.
    Split on /\s/ instead of just whitespace.
    Cleaned up to one var, removed trailing spaces, other things.

--- waves.js
+++ waves.js
@@ -1,35 +1,54 @@
-var amplitude = 36;
-var frequency = Math.PI / 17;
-var offset = 75;
-var phase = 0,
+var escapeRegexes,
+    escapeKeys,
+    amplitude = 36,
+    frequency = Math.PI / 17,
+    offset = 75,
+    phase = 0,
     phase_increment = Math.PI / 36;
 
+escapeRegexes = {
+  "&nbsp": /&/g,
+  "&lt;": /</g,
+  "&gt;": />/g,
+  "&quot;": /"/g,
+  "&#39;": /'/g,
+};
+escapeKeys = Object.keys(escapeRegexes);
+
 function update() {
-  document.getElementById("left").innerHTML = "";
   var length = 0,
-      x = 0;
-  var i;
-  
-  var words = content.split(' ');
-  var lines = [];
-  
+      x = 0,
+      i, words, lines, word, text;
+
+  document.getElementById("left").innerHTML = "";
+  words = content.split(/\s/);
+  lines = [];
+
   for (i = 0; i < words.length; i++) {
-    if (!lines[x])
+    if (!lines[x]) {
       lines[x] = '';
-      
-    var word = words[i] + '&nbsp;';
+    }
+
+    word = clean(words[i]) + '&nbsp;';
     lines[x] += word;
-      
+
     length += word.length;
     if (length >= (offset + Math.sin(phase + x * frequency) * amplitude)) {
       x++;
       length = 0;
     }
   }
-  
-  var text = lines.join('<br>');
+
+  text = lines.join('<br>');
   document.getElementById("left").innerHTML = text;
   document.getElementById("right").innerHTML = text;
-  
+
   phase += phase_increment;
+}
+
+function clean(word) {
+  escapeKeys.forEach(function (key) {
+    word = word.replace(escapeRegexes[key], key);
+  });
+  return word;
 }
